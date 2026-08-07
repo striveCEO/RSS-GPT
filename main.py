e8019b5d4b96ebe2d95216886e4f51e6e9357bfb
striveCEO <61260933+striveCEO@users.noreply.github.com>
2026-08-07 14:01:42 +0800

    Update main.py
    更改模型和AI服务提供商

--- main.py
+++ main.py
@@ -269,13 +269,13 @@ def output(sec, language):
             elif OPENAI_API_KEY:
                 token_length = len(cleaned_article)
                 try:
-                    entry.summary = gpt_summary(cleaned_article,model="openrouter/free", language=language)
+                    entry.summary = gpt_summary(cleaned_article,model="deepseek-v4-flash", language=language)
                     with open(log_file, 'a') as f:
                         f.write(f"Token length: {token_length}\n")
                         f.write(f"Summarized using openrouter-free\n")
                 except:
                     try:
-                        entry.summary = gpt_summary(cleaned_article,model="openrouter/free", language=language)
+                        entry.summary = gpt_summary(cleaned_article,model="deepseek-v4-flash", language=language)
                         with open(log_file, 'a') as f:
                             f.write(f"Token length: {token_length}\n")
                             f.write(f"Summarized using openrouter-free\n")
@@ -314,8 +314,8 @@ max_entries = 1000
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 U_NAME = os.environ.get('U_NAME')
 OPENAI_PROXY = os.environ.get('OPENAI_PROXY')
-#更改为openrouter的免费模型
-OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL', 'https://openrouter.ai/api/v1/chat/completions')
+#更改为免费模型
+OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL', 'https://model.inferx.net/endpoints/v1')
 deployment_url = f'https://{U_NAME}.github.io/RSS-GPT/'
 BASE =get_cfg('cfg', 'BASE')
 keyword_length = int(get_cfg('cfg', 'keyword_length'))
